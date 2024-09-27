375e8a089f81fea208a800b1d09af891a0a93aeb
Olegsandrik <Nikitinnvlalex@gmail.com>
2024-09-27 15:35:47 +0300

    fix certs

--- proxy.go
+++ proxy.go
@@ -46,11 +46,8 @@ func (p *ProxyServer) handleConnect(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *ProxyServer) handleHTTPSConn(w http.ResponseWriter, r *http.Request) {
-
 	hostWithPort := r.Host
 	host := strings.Split(hostWithPort, ":")[0]
-
-	w.WriteHeader(http.StatusOK)
 
 	hij, ok := w.(http.Hijacker)
 	if !ok {
@@ -58,36 +55,72 @@ func (p *ProxyServer) handleHTTPSConn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	conn, _, err := hij.Hijack()
+	connClient, _, err := hij.Hijack()
 	if err != nil {
-		log.Println("Ошибка при захвате соединения:", err)
+		http.Error(w, "Error hijacking connection", http.StatusServiceUnavailable)
+		return
+	}
+	defer connClient.Close()
+
+	_, err = connClient.Write([]byte("HTTP/1.0 200 Connection established\r\n\r\n"))
+	if err != nil {
+		log.Println("Failed to send connection established:", err)
 		return
 	}
 
-	p.genCertificate(host)
-
-	connServ, err := net.Dial("tcp", hostWithPort)
+	connServ, err := net.Dial("tcp", r.Host)
 	if err != nil {
-		log.Fatal("Не удалось установить TLS-соединение с сервером")
+		log.Println("Failed to connect to destination:", err)
+		return
 	}
+	defer connServ.Close()
+
+	certPair, err := p.genCertificate(host)
+	if err != nil {
+		log.Print("Failed to load certificate ", err)
+		return
+	}
+	log.Print("Successfully loaded certificate")
+
+	tlsConf := &tls.Config{
+		Certificates: []tls.Certificate{certPair},
+		MinVersion:   tls.VersionTLS13,
+		MaxVersion:   tls.VersionTLS13,
+	}
+
+	tlsConnClient := tls.Server(connClient, tlsConf)
+	err = tlsConnClient.Handshake()
+	if err != nil {
+		log.Println("TLS handshake with client failed:", err)
+		return
+	}
+	defer tlsConnClient.Close()
+
+	tlsConnServ := tls.Client(connServ, &tls.Config{InsecureSkipVerify: true})
+	err = tlsConnServ.Handshake()
+	if err != nil {
+		log.Println("TLS handshake with server failed:", err)
+		return
+	}
+	defer tlsConnServ.Close()
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	go func() {
 		defer wg.Done()
-		_, _ = io.Copy(connServ, conn)
+		_, _ = io.Copy(tlsConnServ, tlsConnClient)
 	}()
 
 	go func() {
 		defer wg.Done()
-		_, _ = io.Copy(conn, connServ)
+		_, _ = io.Copy(tlsConnClient, tlsConnServ)
 	}()
 
 	wg.Wait()
 
-	conn.Close()
-	connServ.Close()
+	tlsConnClient.Close()
+	tlsConnServ.Close()
 }
 
 func (p *ProxyServer) genCertificate(host string) (tls.Certificate, error) {
@@ -95,30 +128,35 @@ func (p *ProxyServer) genCertificate(host string) (tls.Certificate, error) {
 	counter++
 	serialNumber := strconv.Itoa(counter)
 
+	// Генерация сертификата
 	cmd := exec.Command("./gen_cert.sh", commonName, serialNumber)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Printf("Command execution failed: %s\nOutput: %s\n", err, output)
 		return tls.Certificate{}, fmt.Errorf("failed to generate certificate: %w", err)
 	}
-	log.Printf("Certificate generation output: %s\n", output)
 
-	cert, err := ioutil.ReadFile("cert.crt")
+	// Чтение сертификата и ключа
+	cert, err := ioutil.ReadFile("certs/" + host + ".crt")
 	if err != nil {
 		return tls.Certificate{}, fmt.Errorf("failed to read cert.crt: %w", err)
 	}
-
-	key, err := ioutil.ReadFile("cert.key")
+	key, err := ioutil.ReadFile("certs/" + host + ".key")
 	if err != nil {
 		return tls.Certificate{}, fmt.Errorf("failed to read cert.key: %w", err)
 	}
+
+	/*
+		defer os.Remove("certs/" + host + ".crt")
+		defer os.Remove("certs/" + host + ".key")
+	*/
 
 	certPair, err := tls.X509KeyPair(cert, key)
 	if err != nil {
 		return tls.Certificate{}, fmt.Errorf("failed to parse certificate pair: %w", err)
 	}
 
-	// log.Println("Successfully generated certificate for:", commonName)
+	log.Println("Successfully used certificate for:", commonName)
 	return certPair, nil
 }
 
